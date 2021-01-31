0b16904fa3ab3c903170be22b2fa3b4b911490c5
Tushar V Shah <tshah132@gmail.com>
2021-01-31 12:05:43 -0800

    Added Event handler, form submission and callback

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,32 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
-    onInputChange() {}
+    state = { term: '' }
+
+    //92: Solving Content issues: converting onFormSubmit(event) {} to onFormSubmit = (event) => {} binds
+    //'this' reference in the function to instance of SearchBar
+    onFormSubmit = (event) => {
+        //onFormSubmit(event) {
+        //prevents form submission after user hits enter
+        //form submission forces the page to refresh
+        event.preventDefault()
+        this.props.onSubmit(this.state.term)
+    }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Input Search</label>
-                        <input type="text" onChange={this.onInputChange} />
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={(event) =>
+                                //this.render is called everytime this.setState is called
+                                this.setState({ term: event.target.value })
+                            }
+                        />
                     </div>
                 </form>
             </div>
