049a4fc2bcae97ab82954e8433a0b228993b1bca
Tushar V Shah <tshah132@gmail.com>
2021-05-05 21:03:08 -0700

    Add environment variables

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://ec2-13-59-129-251.us-east-2.compute.amazonaws.com:8080',
-    // baseURL: 'http://localhost:8080',
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     headers: {
         Authorization:
             'Client-ID 9ab4afbc5c5b75037dba3fdc0dbbe6235491836a663a354a66522f1c75c8ea63',
