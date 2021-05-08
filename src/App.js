cea8ad0fb001371142c366024c24678bbd241961
Tushar V Shah <tshah132@gmail.com>
2021-05-08 06:58:37 -0700

    Make Fixed menu Component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
 import React from 'react';
 import Home from './components/Home.js';
 import About from './components/About.js';
-import GA from './utils/RouteChangeTracker.js';
-import moment from 'moment';
+import FixedMenu from './components/FixedMenu';
 
-import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import GA from './utils/RouteChangeTracker.js';
+
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
-    let timeInUTC = moment().utc().format('ddd, MMMM Do YYYY, h:mm a');
     return (
         <Router>
             <div>
-                <div className="ui fixed inverted menu">
-                    <div className="ui container">
-                        <Link to="/" className="header item">
-                            <img
-                                className="logo"
-                                alt="datamath.net logo"
-                                src="logo12x12.png"
-                            />
-                            dateMath
-                        </Link>
-                        <Link to="/" className="item">
-                            Home
-                        </Link>
-                        <Link to="/about" className="item">
-                            About
-                        </Link>
-                        <div className="right menu">
-                            <Link to="/" className="item">
-                                UTC:&nbsp;&nbsp;{timeInUTC}
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-
+                <FixedMenu />
                 <Switch>
                     <Route path="/about">
                         <About />
---
+++ src/components/FixedMenu.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import moment from 'moment';
+
+import { Link } from 'react-router-dom';
+
+const FixedMenu = () => {
+    let timeInUTC = moment().utc().format('ddd, MMMM Do YYYY, h:mm a');
+    return (
+        <div className="ui fixed inverted menu">
+            <div className="ui container">
+                <Link to="/" className="header item">
+                    <img
+                        className="logo"
+                        alt="datamath.net logo"
+                        src="logo12x12.png"
+                    />
+                    dateMath
+                </Link>
+                <Link to="/" className="item">
+                    Home
+                </Link>
+                <Link to="/about" className="item">
+                    About
+                </Link>
+                <div className="right menu">
+                    <Link to="/" className="item">
+                        UTC:&nbsp;&nbsp;{timeInUTC}
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FixedMenu;
