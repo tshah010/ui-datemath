828e0fefb6fa11ee66fa60bb863fd2d144d177ff
Tushar V Shah <tshah132@gmail.com>
2021-05-08 08:04:07 -0700

    Add footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './components/Home.js';
 import About from './components/About.js';
 import FixedMenu from './components/FixedMenu';
+import Footer from './components/Footer';
 
 import GA from './utils/RouteChangeTracker.js';
 
@@ -11,6 +12,7 @@ const App = () => {
     return (
         <Router>
             <div>
+                {/* show menu */}
                 <FixedMenu />
                 <Switch>
                     <Route path="/about">
@@ -21,6 +23,8 @@ const App = () => {
                         <Home />
                     </Route>
                 </Switch>
+                {/* show footer */}
+                <Footer />
             </div>
         </Router>
     );
---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, Grid, Segment } from 'semantic-ui-react';
+
+// Found in https://github.com/Semantic-Org/Semantic-UI-React/blob/master/docs/src/layouts/FixedMenuLayout.js
+const Footer = () => (
+    <div className="dt footer">
+        <Segment inverted vertical>
+            <Container textAlign="left">
+                <Grid divided inverted stackable>
+                    <Grid.Column width={12}>
+                        {' '}
+                        &copy; 2021 DateMath.net
+                    </Grid.Column>
+                    <Grid.Column width={4}></Grid.Column>
+                </Grid>
+            </Container>
+        </Segment>
+    </div>
+);
+
+export default Footer;
